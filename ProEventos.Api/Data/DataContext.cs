e3e33bd97004de08ade0d36f9b1efbd9ed6cc5c0
msousa <matheussousa0241@gmail.com>
2024-08-01 22:10:07 -0300

    Add db context

---
+++ ProEventos.Api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
